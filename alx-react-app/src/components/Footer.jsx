2d2fd0eed6d4e236515cb199648b827e8dbb9b93
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-08-11 01:42:35 +0000

    header , maincontent and footer components added

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default footer;
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function mainContent() {
+  return (
+    <main>
+      <p>I love to visit New York, paris, ad Tokyo.</p>
+    </main>
+  );
+}
+export default mainContent;
