a9f84709f2b9ad829a8e7d9f991064bfe30949c6
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-08-11 01:29:22 +0000

    small changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from "./components/WelcomeMessage.jsx";
+import WelcomeMessage from "./components/welcomeMessage";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
