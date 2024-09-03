89cb4568610fac6e5609c67ff2f6bae59963c2ca
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-09-03 09:42:10 +0000

    alx react app new created and modified

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ borderBottom: "1px solid green", fontWeight: "bold" }}>
+      <p>&copy; 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
