3fb9ad4a32f862474ddbb93d11a09d478c9cb5b5
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-09-03 09:56:35 +0000

    included increment and decrement

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,8 +6,8 @@ export function Counter() {
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
