9736c0e31dc00590703a4eb9954eac80e8f7d248
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-09-04 12:31:14 +0000

    styles added

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,19 +3,47 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <nav style={{ backgroundColor: "#282c34", padding: "10px" }}>
+      <ul
+        style={{
+          listStyle: "none",
+          margin: "0",
+          padding: "0",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <li style={{ margin: "0 15px" }}>
+          <Link
+            to="/"
+            style={{ color: "white", textDecoration: "none", fontSize: "18px" }}
+          >
+            Home
+          </Link>
         </li>
-        <li>
-          <Link to="/about">About</Link>
+        <li style={{ margin: "0 15px" }}>
+          <Link
+            to="/about"
+            style={{ color: "white", textDecoration: "none", fontSize: "18px" }}
+          >
+            About
+          </Link>
         </li>
-        <li>
-          <Link to="/services">Services</Link>
+        <li style={{ margin: "0 15px" }}>
+          <Link
+            to="/services"
+            style={{ color: "white", textDecoration: "none", fontSize: "18px" }}
+          >
+            Services
+          </Link>
         </li>
-        <li>
-          <Link to="/contact">Contact</Link>
+        <li style={{ margin: "0 15px" }}>
+          <Link
+            to="/contact"
+            style={{ color: "white", textDecoration: "none", fontSize: "18px" }}
+          >
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
