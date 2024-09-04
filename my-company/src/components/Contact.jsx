078f5a1efd750f722ecaf5d2a831286f7903c0f8
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-09-04 12:34:14 +0000

    return statement fixed

--- my-company/src/components/Contact.jsx
+++ my-company/src/components/Contact.jsx
@@ -15,39 +15,38 @@ function Contact() {
     e.preventDefault();
     alert("Form Submitted!");
   };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+          style={{ display: "block", margin: "10px 0" }}
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+          style={{ display: "block", margin: "10px 0" }}
+        />
+        <textarea
+          name="message"
+          placeholder="Your Message"
+          value={formData.message}
+          onChange={handleChange}
+          style={{ display: "block", margin: "10px 0" }}
+        />
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  );
 }
-
-return (
-  <div style={{ padding: "20px" }}>
-    <h1>Contact Us</h1>
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Your Email"
-        value={formData.email}
-        onChange={handleChange}
-        style={{ display: "block", margin: "10px 0" }}
-      />
-
-      <input
-        type="email"
-        name="email"
-        placeholder="Your Email"
-        value={formData.email}
-        onChange={handleChange}
-        style={{ display: "block", margin: "10px 0" }}
-      />
-      <textarea
-        name="message"
-        placeholder="Your Message"
-        value={formData.message}
-        onChange={handleChange}
-        style={{ display: "block", margin: "10px 0" }}
-      />
-      <button type="submit">Send Message</button>
-    </form>
-  </div>
-);
-
 export default Contact;
