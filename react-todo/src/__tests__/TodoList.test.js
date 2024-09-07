ed51eaf3b107ad8d11ba6344c4f87e714090696a
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-09-07 15:22:26 +0000

    package.json updated

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -6,28 +6,35 @@ import TodoList from "../TodoList";
 
 test("renders TodoList component", () => {
   render(<TodoList />);
+  // Check that initial todos are rendered
   expect(screen.getByText("Learn React")).toBeInTheDocument();
   expect(screen.getByText("Learn Jest")).toBeInTheDocument();
 });
 
 test("adds a new todo", () => {
   render(<TodoList />);
+  // Add a new todo
   fireEvent.change(screen.getByRole("textbox"), {
     target: { value: "New Todo" },
   });
   fireEvent.click(screen.getByText("Add Todo"));
+  // Check if new todo is added
   expect(screen.getByText("New Todo")).toBeInTheDocument();
 });
 
 test("toggles a todo item", () => {
   render(<TodoList />);
   const todo = screen.getByText("Learn React");
+  // Toggle todo status
   fireEvent.click(todo);
+  // Check if the todo item is toggled (crossed out)
   expect(todo).toHaveStyle("text-decoration: line-through");
 });
 
 test("deletes a todo item", () => {
   render(<TodoList />);
+  // Delete the first todo item
   fireEvent.click(screen.getByText("Delete", { selector: "button" }));
+  // Check if the todo item is removed
   expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
 });
