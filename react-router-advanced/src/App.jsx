9d0a10af42d22fb01d11244374b279646599ea8f
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-09-07 14:49:58 +0000

    app.jsx updated

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,3 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import NotFound from "./components/NotFound";
+import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -19,3 +25,5 @@ function App() {
     </Router>
   );
 }
+
+export default App;
