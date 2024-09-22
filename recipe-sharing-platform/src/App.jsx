213d169bfb3c7a1cd464cb1dc845f4983895d2d9
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-09-22 21:06:41 +0000

    nav added

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./components/Homepage";
 import RecipeDetail from "./components/RecipeDetail";
 
 export default function App() {
   return (
     <Router>
+      <nav classname="bg-gray-800 p-4">
+        <Link to="/" classname="text-white mr-4">
+          Home
+        </Link>
+      </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
