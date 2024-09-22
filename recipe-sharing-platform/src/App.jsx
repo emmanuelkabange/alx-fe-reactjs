e907885c34df7b19546dc27846ee7849d5535139
Emmanuel Kabange <emmanuelwelek@gmail.com>
2024-09-22 19:29:24 +0000

    reverted to a stable working version

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,6 @@
 import { Route, Router } from "react-router-dom";
 import HomePage from "./components/Homepage";
-import RecipeDetail from "./components/RecipeDetail";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-      </Routes>
-    </Router>
-  );
+  return <HomePage />;
 }
